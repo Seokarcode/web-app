0a881fd49d4bbe43226b3e3ebc9c2c668a4a0f68
Seokarcode <sajjadakbariiii415@gmail.com>
2025-01-22 19:35:03 +0330

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'seokar-web-app-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.php',
+  // Add other URLs of your site that you want to cache
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
